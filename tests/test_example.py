1c21526c12890a7e1fcf124263cb55c3b8360a5f
Antony Zappacosta <antony.zappacosta@studio.unibo.it>
2024-08-27 11:44:03 +0200

    commit to try github action again

--- tests/test_example.py
+++ tests/test_example.py
@@ -1,6 +1,9 @@
 class TestClass:
     def test_always_passes(self):
         assert True
+    
+    def second_test_always_passes(self):
+        assert True
 
     #def test_always_fails(self):
     #    assert False
